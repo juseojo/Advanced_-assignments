81a974ded498095d400c0f63fc88cb55f32e831b
juseojo <j86371224@gmail.com>
2025-06-02 20:33:33 +0900

    Feat: 3번 과제 구현

---
+++ Advanced_ assignments/Question3/Filters.swift
@@ -0,0 +1,40 @@
+//
+//  Filters.swift
+//  Advanced_ assignments
+//
+//  Created by seongjun cho on 6/2/25.
+//
+
+func a(array: [Int]) -> [Int] {
+	var count = 1
+	return array.filter {_ in
+		count += 1
+		return count % 2 == 0
+	}
+}
+
+func b(array: [String]) -> [String] {
+	var count = 1
+	return array.filter {_ in
+		count += 1
+		return count % 2 == 0
+	}
+}
+
+func c<T: Sequence>(array: T) -> T {
+	var count = 1
+
+	return array.filter {_ in
+		count += 1
+		return count % 2 == 0
+	} as! T
+}
+
+func d<T: Sequence>(array: T) -> T where T.Element: Numeric {
+	var count = 1
+
+	return array.filter {_ in
+		count += 1
+		return count % 2 == 0
+	} as! T
+}
