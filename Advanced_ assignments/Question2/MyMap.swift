ce65eeb1b63c15768aacc24053ff71122a450476
juseojo <j86371224@gmail.com>
2025-06-02 20:05:30 +0900

    Feat: 2번 과제 구현

---
+++ Advanced_ assignments/Question2/MyMap.swift
@@ -0,0 +1,33 @@
+//
+//  MyMap.swift
+//  Advanced_ assignments
+//
+//  Created by seongjun cho on 6/2/25.
+//
+
+
+let numbers = [1, 2, 3, 4, 5]
+var result = [String]()
+
+func Question2() {
+	// 2-1
+	result = numbers.map { String($0) }
+
+	// 2-2
+	let source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+	print(source
+			.filter { $0 % 2 == 0 }
+			.map { String($0) })
+}
+
+// 2-3
+func myMap(_ array: [Int], trailingClosure: @escaping (Int) -> String) -> [String] {
+	var result = [String]()
+
+	for num in array {
+		result.append(trailingClosure(num))
+	}
+
+	return result
+}
