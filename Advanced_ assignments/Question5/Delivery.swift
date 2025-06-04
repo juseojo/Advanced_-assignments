8119b0dbf138f520935330eb241b5f675d553e98
juseojo <j86371224@gmail.com>
2025-06-04 21:09:44 +0900

    Feat: 5번 과제 개선
    - switch case문으로 개선했고, catch를 다양하게 수정하였다.

--- Advanced_ assignments/Question5/Delivery.swift
+++ Advanced_ assignments/Question5/Delivery.swift
@@ -22,12 +22,15 @@ func predictDeliveryDay(for address: String, status: DeliveryStatus) throws -> S
 
 	if address == "" {
 		throw DeliveryError.invalidAddress
-	} else if status == DeliveryStatus.notStarted {
+	}
+
+	switch status {
+	case .notStarted:
 		throw DeliveryError.notStarted
-	} else if status == DeliveryStatus.error {
-		throw DeliveryError.systemError(reason: "시스템 오류입니다.")
-	} else if case let DeliveryStatus.inTransit(daysRemaining) = status {
+	case .inTransit(let daysRemaining):
 		result = "배송까지 \(daysRemaining)일 남았습니다."
+	case .error:
+		throw DeliveryError.systemError(reason: "시스템 오류입니다.")
 	}
 
 	return result
@@ -37,7 +40,13 @@ func question5() {
 	do {
 		try print(predictDeliveryDay(for: "house", status: .inTransit(daysRemaining: 5)))
 		try print(predictDeliveryDay(for: "", status: .inTransit(daysRemaining: 5)))
+	} catch DeliveryError.invalidAddress {
+		print("주소가 잘못되었습니다.")
+	} catch DeliveryError.notStarted {
+		print("배송이 아직 시작되지 않았습니다.")
+	} catch DeliveryError.systemError(reason: let error) {
+		print(error)
 	} catch {
-		print("error: \(error)")
+		print("오류: \(error)")
 	}
 }
